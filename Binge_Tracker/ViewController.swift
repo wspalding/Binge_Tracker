be437589be6fe495881e201f03cdd61050704ed3
wspalding <thisisspalding@gmail.com>
2019-09-24 22:24:51 -0700

    Initial Commit

---
+++ Binge_Tracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Binge_Tracker
+//
+//  Created by William Spalding on 9/24/19.
+//  Copyright © 2019 William Spalding. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

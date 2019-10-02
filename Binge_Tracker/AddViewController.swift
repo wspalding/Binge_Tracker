a89985c9e94294a5856493a20cdb8b2928ed8975
wspalding <thisisspalding@gmail.com>
2019-10-02 13:12:35 -0700

    searching works (but the api search sucks)

--- Binge_Tracker/AddViewController.swift
+++ Binge_Tracker/AddViewController.swift
@@ -45,43 +45,68 @@ class AddViewController: UIViewController, UITableViewDelegate, UITableViewDataS
     
     func searchForShow(query: String)
     {
-        let image = UIImage(named: "iconfinder_video_call_2639945") ?? UIImage(named: "image_not_found")!
+//        let image = UIImage(named: "iconfinder_video_call_2639945") ?? UIImage(named: "image_not_found")!
         
         // make HTTPS request here
         let key = "5b22867e"
-//        let key = "bea0628b-f412-44e7-aba8-16ad02bf6f38"
-//        let session = URLSession.shared
         let query_str = query.replacingOccurrences(of: " ", with: "%20")
-        let url = URL(string: "https://omdbapi.com/?apikey=" + key + "&s=" + query_str)!
-//        print(url)
-        
-        let task = URLSession.shared.dataTask(with: url)
-        { (data, response, error) in
-            
-            if let data = data
+        if let url = URL(string: "https://omdbapi.com/?apikey=" + key + "&s=" + query_str)
+        {
+            let task = URLSession.shared.dataTask(with: url)
             {
-                do
+                (data, response, error) in
+                if let data = data
                 {
-                    // Convert the data to JSON
-                    let jsonSerialized = try JSONSerialization.jsonObject(with: data, options: []) as? [String : Any]
-                    
-                    print("json: ", jsonSerialized!)
-                   
+                    do
+                    {
+                        // Convert the data to JSON
+                        if let jsonSerialized = try JSONSerialization.jsonObject(with: data, options: []) as? [String : Any]
+                        {
+//                            print(jsonSerialized)
+                            if let result = jsonSerialized["Response"]
+                            {
+                                if result as! String == "True"
+                                {
+                                    if let searches = jsonSerialized["Search"] as? [[String : String]]
+                                    {
+                                        self.searchReasults = []
+                                        for search in searches
+                                        {
+                                            let title = search["Title"]
+                                            let poster_url = search["Poster"]
+                                            var poster = UIImage(named: "image_not_found")!
+                                            if (poster_url != nil)
+                                            {
+                                                let url = URL(string: poster_url!)
+                                                let data = try? Data(contentsOf: url!)
+                                                //make sure your image in this url does exist, otherwise unwrap in a if let check / try-catch
+                                                if (data != nil)
+                                                {
+                                                    poster = UIImage(data: data!) ?? UIImage(named: "image_not_found")!
+                                                }
+                                            }
+                                            self.searchReasults.append((title ?? "N/A", poster))
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                        //                    print("json: ", jsonSerialized!)
+                    }
+                    catch let error as NSError
+                    {
+                        print(error.localizedDescription)
+                    }
                 }
-                catch let error as NSError
+                else if let error = error
                 {
                     print(error.localizedDescription)
                 }
             }
-            else if let error = error
-            {
-                print(error.localizedDescription)
-            }
+            task.resume()
         }
-        
-        task.resume()
-        
-        searchReasults = [("test", image)]
+//        print(url) le
+//        searchReasults = [("test", image)]
         
     }
     
